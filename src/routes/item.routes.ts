9767d086070c0356ad5206ef2d0f9660262027fc
Shashank <shashanksakhre01@gmail.com>
2026-05-21 00:11:57 +0530

    added comments

--- src/routes/item.routes.ts
+++ src/routes/item.routes.ts
@@ -14,3 +14,13 @@ router.post("/", authenticateUser, createItemController);
 router.get("/", getAllItemsController);
 
 export default router;
+
+
+/*
+
+http://localhost:5000/api/v1/items?page=3&limit=2
+http://localhost:5000/api/v1/items?sort=price_asc
+http://localhost:5000/api/v1/items?minPrice=10000&maxPrice=70000
+http://localhost:5000/api/v1/items?search=iphone
+http://localhost:5000/api/v1/items
+*/
